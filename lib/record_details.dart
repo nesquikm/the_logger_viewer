f89ceb85195771d341bb81ce1d1b0e276c6bb7e7
nesquikm <drapolyuk@gmail.com>
2024-09-04 13:34:31 +0400

    refactor: optimize RecordDetails widget layout

--- lib/record_details.dart
+++ lib/record_details.dart
@@ -15,70 +15,67 @@ class RecordDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Stack(
-    // fit: StackFit.expand,
-    // children: [
-    // return
-    // ColoredBox(
-    // color: record.level.color.withOpacity(0.4),
-    // child:
-    // return SelectionArea(
-    // child:
-    return SingleChildScrollView(
-      child: Table(
-        columnWidths: const {
-          0: IntrinsicColumnWidth(),
-          1: FlexColumnWidth(),
-        },
-        children: [
-          _row(
-            'Session id',
-            record.sessionId.toString(),
-          ),
-          _row(
-            'Id',
-            record.id.toString(),
-          ),
-          _row(
-            'Timestamp',
-            '${record.recordTimestamp} (${record.time})',
-          ),
-          _row(
-            'Logger name',
-            record.loggerName,
-          ),
-          _row(
-            'Level',
-            record.level.name.toLowerCase(),
-          ),
-          _row(
-            'Message',
-            record.message,
-          ),
-          if (record.error != null)
-            _row(
-              'Error',
-              record.error!,
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        ColoredBox(
+          color: record.level.color.withOpacity(0.4),
+          child: SelectionArea(
+            child: SingleChildScrollView(
+              child: Table(
+                columnWidths: const {
+                  0: IntrinsicColumnWidth(),
+                  1: FlexColumnWidth(),
+                },
+                children: [
+                  _row(
+                    'Session id',
+                    record.sessionId.toString(),
+                  ),
+                  _row(
+                    'Id',
+                    record.id.toString(),
+                  ),
+                  _row(
+                    'Timestamp',
+                    '${record.recordTimestamp} (${record.time})',
+                  ),
+                  _row(
+                    'Logger name',
+                    record.loggerName,
+                  ),
+                  _row(
+                    'Level',
+                    record.level.name.toLowerCase(),
+                  ),
+                  _row(
+                    'Message',
+                    record.message,
+                  ),
+                  if (record.error != null)
+                    _row(
+                      'Error',
+                      record.error!,
+                    ),
+                  if (record.stackTrace != null)
+                    _row(
+                      'Stack trace',
+                      record.stackTrace!,
+                    ),
+                ],
+              ),
             ),
-          if (record.stackTrace != null)
-            _row(
-              'Stack trace',
-              record.stackTrace!,
-            ),
-        ],
-      ),
-      // ),
-      // ),
-      // ),
-      // Align(
-      // alignment: Alignment.topRight,
-      // child: IconButton(
-      // onPressed: _onCopy,
-      // icon: const Icon(Icons.copy),
-      // tooltip: 'Copy to clipboard',
-      // ),
-      // ),
-      // ],
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: IconButton(
+            onPressed: _onCopy,
+            icon: const Icon(Icons.copy),
+            tooltip: 'Copy to clipboard',
+          ),
+        ),
+      ],
     );
   }
 
@@ -87,15 +84,11 @@ class RecordDetails extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          child: TableCell(
-            child: Text('$label '),
-          ),
+          child: Text('$label '),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          child: TableCell(
-            child: Text('$value '),
-          ),
+          child: Text('$value '),
         ),
       ],
     );
